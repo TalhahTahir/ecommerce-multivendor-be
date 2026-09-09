d3e55fe973fc02685d0d38c1c9c13a72c1868c64
Talha Tahir <mr.talhahtahir@gmail.com>
2026-09-09 19:53:55 +0500

    Update activation URL to production environment

--- controller/user.js
+++ controller/user.js
@@ -35,7 +35,7 @@ router.post("/create-user", async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `http://localhost:5173/activation/${activationToken}`;
+    const activationUrl = `https://ecommerce-multivendor-fe-vydh.vercel.app//activation/${activationToken}`;
 
     try {
       await sendMail({
