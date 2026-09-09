31e54b8d42901ee80ffdae466ecad89131d2b7af
Talha Tahir <mr.talhahtahir@gmail.com>
2026-09-09 21:04:27 +0500

    Update activation URL for seller activation

--- controller/shop.js
+++ controller/shop.js
@@ -39,7 +39,7 @@ router.post("/create-shop", catchAsyncErrors(async (req, res, next) => {
 
     const activationToken = createActivationToken(seller);
 
-    const activationUrl = `https://ecommerce-multivendor-fe-vydh.vercel.app//seller/activation/${activationToken}`;
+    const activationUrl = `https://ecommerce-multivendor-fe-vydh-pvbrvaxew-talhahtahirs-projects.vercel.app/seller/activation/${activationToken}`;
 
     try {
       await sendMail({
